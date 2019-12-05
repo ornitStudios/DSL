e0eac35d50998b85e067fd84fbb50df49c2f5b8d
jrichalot <JRICHALOT@asas.qld.edu.au>
2019-12-05 12:37:49 +1000

    Adding static file service

--- Unit 3/Selfie App/index.js
+++ Unit 3/Selfie App/index.js
@@ -1,4 +1,4 @@
-// importin express
+// importing express
 const express = require('express');
 
 // create a web application as an instance of express()
@@ -8,3 +8,7 @@ const app = express();
 // 1. a port on which to listen
 // 2. a callback function i.e. what to do when a request arrives through this port
 app.listen(3000, () => console.log('listening on 3000'));
+
+// use the static function of the express library.
+// the function takes a file or a directory as a parameter
+app.use(express.static('public'));
