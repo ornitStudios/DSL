72b6f360985ce2867b1024b44893685abf8f1a4a
jrichalot <JRICHALOT@asas.qld.edu.au>
2019-12-05 14:22:50 +1000

    Testing posting to server

--- Unit 3/Selfie App/index.js
+++ Unit 3/Selfie App/index.js
@@ -12,3 +12,11 @@ app.listen(3000, () => console.log('listening on 3000'));
 // use the static function of the express library.
 // the function takes a file or a directory as a parameter
 app.use(express.static('public'));
+
+// create a route to 'api" via the the POST method
+// request has all the http request from the client
+// response is the variable the server will send back to the client
+app.post('/api', (request,response) => {
+    // Console.log the request from the client to the server console to check
+    console.log(request);
+}); 
