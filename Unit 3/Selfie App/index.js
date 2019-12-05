3d462c1d20271063556c302970a8254488db2485
jrichalot <JRICHALOT@asas.qld.edu.au>
2019-12-05 14:43:34 +1000

    Adding express.json() and parsing client request

--- Unit 3/Selfie App/index.js
+++ Unit 3/Selfie App/index.js
@@ -13,10 +13,15 @@ app.listen(3000, () => console.log('listening on 3000'));
 // the function takes a file or a directory as a parameter
 app.use(express.static('public'));
 
+// give the server the ability to received and parse json
+// options are passed as argusments e.g. no json file bigger than 1mb
+app.use(express.json({limit: '1mb'}));
+
 // create a route to 'api" via the the POST method
 // request has all the http request from the client
 // response is the variable the server will send back to the client
 app.post('/api', (request,response) => {
     // Console.log the request from the client to the server console to check
-    console.log(request);
+    console.log('I got a request!');
+    console.log(request.body);
 }); 
