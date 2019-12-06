b489ee7ddd22588564ff41db3cc640fbbfe2fae4
jrichalot <JRICHALOT@asas.qld.edu.au>
2019-12-06 18:05:16 +1000

    creating an NeDB database and writing to it.

--- Unit 3/Selfie App/index.js
+++ Unit 3/Selfie App/index.js
@@ -4,6 +4,9 @@ const express = require('express');
 // create a web application as an instance of express()
 const app = express();
 
+// importing nedb
+const Datastore = require('nedb');
+
 // listen! the listen() function takes two arguments
 // 1. a port on which to listen
 // 2. a callback function i.e. what to do when a request arrives through this port
@@ -17,8 +20,10 @@ app.use(express.static('public'));
 // options are passed as argusments e.g. no json file bigger than 1mb
 app.use(express.json({limit: '1mb'}));
 
-// creating an arry to write to
-const database = [];
+// create an database to write to
+const database = new Datastore('./data/database.db');
+// load the database into memory. if it does not exist create the db.
+database.loadDatabase();
 
 // create a route to 'api" via the the POST method
 // request has all the http request from the client
@@ -28,14 +33,17 @@ app.post('/api', (request,response) => {
     console.log('I got a request!');
 
     const data = request.body
-
+    // create a timestamp
+    const timestamp = Date.now();
+    // assign timestamp to data object
+    data.timestamp = timestamp;
     // pushing data to the array
-    database.push(data);
-    console.log(database);
+    database.insert(data);
 
     // complete the response
     response.json({
         status: 'success',
+        timestamp: timestamp,
         latitude: request.body.lat,
         longitude: request.body.lon
     });
